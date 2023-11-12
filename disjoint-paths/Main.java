21bd1263763210660363eaec1cfb094482dcaf96
Eduardo Lemos <dudulemox@gmail.com>
2023-11-12 17:43:54 -0300

    test format

--- disjoint-paths/Main.java
+++ disjoint-paths/Main.java
@@ -5,30 +5,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        // Make sure you pass the file path correctly
-        String file_path = "./input/testing.txt";
+        // String file_path = "";
 
-        try (FileReader in = new FileReader(file_path)) {
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Enter the file path: ");
+        // file_path = scanner.nextLine();
+        // }
+        // file_path = "./input/" + file_path + ".txt";
 
-            try (Scanner sc = new Scanner(in)) {
+        for (int i = 0; i < 11; i++) {
+            long startTime = System.currentTimeMillis();
 
-                int origin = sc.nextInt();
-                int destination = sc.nextInt();
-                int vertex_amt = sc.nextInt();
-                int edge_amt = sc.nextInt();
+            try (FileReader in = new FileReader("./input/cicle_100.txt")) {
 
-                Graph graph = new Graph(vertex_amt, origin, destination);
+                try (Scanner sc = new Scanner(in)) {
 
-                while (sc.hasNextInt()) {
-                    int v = sc.nextInt();
-                    int w = sc.nextInt();
+                    int origin = sc.nextInt();
+                    int destination = sc.nextInt();
+                    int vertex_amt = sc.nextInt();
+                    int edge_amt = sc.nextInt();
 
-                    graph.insert_edge(v, w);
+                    Graph graph = new Graph(vertex_amt, origin, destination);
+
+                    while (sc.hasNextInt()) {
+                        int v = sc.nextInt();
+                        int w = sc.nextInt();
+
+                        graph.insert_edge(v, w);
+                    }
+
+                    graph.get_disjoint_paths();
                 }
-
-                graph.get_disjoint_paths();
             }
-        }
 
+            long endTIme = System.currentTimeMillis();
+
+            System.out.println("Took: " + (endTIme - startTime) + "ms");
+        }
     }
+
 }
